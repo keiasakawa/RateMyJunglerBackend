d2538303845157a796afc999fc6b677f5abe2bcc
Kei Asakawa <77090883+Kaiyooon@users.noreply.github.com>
2024-03-07 09:56:56 -0800

    secret env

--- routes/info.js
+++ routes/info.js
@@ -1,12 +1,13 @@
-import {config} from '../config.js'
-
-import express from 'express'
+import express from "express";
 var router = express.Router();
-import makeAPIRequest from '../api_request.js'
+import makeAPIRequest from "../api_request.js";
 
-router.get('/:id', function(req,res) {
-    var id = req.params.id;
-    makeAPIRequest(`https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/${id}?api_key=${config.API_KEY}`, res)
-})
+router.get("/:id", function (req, res) {
+  var id = req.params.id;
+  makeAPIRequest(
+    `https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/${id}?api_key=${process.env.API_KEY}`,
+    res
+  );
+});
 
 export default router;
--- routes/profile.js
+++ routes/profile.js
@@ -1,13 +1,13 @@
-import {config} from '../config.js'
-
-import express from 'express'
+import express from "express";
 var router = express.Router();
-import makeAPIRequest from '../api_request.js'
+import makeAPIRequest from "../api_request.js";
 
-router.get('/summoner/:name', function(req,res) {
-    var name = req.params.name;
-    makeAPIRequest(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${name}?api_key=${config.API_KEY}`, res)
-})
-
+router.get("/summoner/:name", function (req, res) {
+  var name = req.params.name;
+  makeAPIRequest(
+    `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${name}?api_key=${process.env.API_KEY}`,
+    res
+  );
+});
 
 export default router;
